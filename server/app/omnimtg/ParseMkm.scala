5833e800cf1196351cf1398dd10d68f76bde9f28
karsten <karsten@magemarket.com>
2019-11-29 14:46:02 +0100

    condition codes

--- server/app/omnimtg/ParseMkm.scala
+++ server/app/omnimtg/ParseMkm.scala
@@ -28,10 +28,11 @@ object ParseMkm {
   val allConditionsData = Array(
     Entry(name = "Damaged", twoLetterCode = "PO", id = 1, usName = "Damaged"),
     Entry(name = "Played", twoLetterCode = "PL", id = 2, usName = "Played"),
+    Entry(name = "LP", twoLetterCode = "LP", id = 2, usName = "Played"),
     Entry(name = "Good", twoLetterCode = "GD", id = 3, usName = "Moderately Played"),
     Entry(name = "Excellent", twoLetterCode = "EX", id = 4, usName = "Lightly Played"),
-    Entry(name = "LP", twoLetterCode = "LP", id = 4, usName = "Lightly Played"),
-    Entry(name = "Near Mint", twoLetterCode = "NM", id = 5, usName = "Near Mint")
+    Entry(name = "Near Mint", twoLetterCode = "NM", id = 5, usName = "Near Mint"),
+    Entry(name = "Mint", twoLetterCode = "MT", id = 5, usName = "Mint")
   );
 
   val allFoilsData = Array(
