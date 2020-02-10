aac5a11e4b7b8ee940afdea562b81856453a2d67
karsten <karsten@magemarket.com>
2020-02-10 12:21:50 +0100

    close connection

--- server/app/omnimtg/SnapConnector.scala
+++ server/app/omnimtg/SnapConnector.scala
@@ -5,7 +5,7 @@ import java.io._
 import java.net.{HttpURLConnection, URL}
 
 class SnapConnector(func: NativeFunctionProvider) {
-  def call(mc:MainController, requestURL: String, method: String, auth: String = null, body: String = null): String = {
+  def call(mc: MainController, requestURL: String, method: String, auth: String = null, body: String = null): String = {
 
     mc.snapCallsSoFar.setValue(mc.snapCallsSoFar.getValue + 1)
 
@@ -25,59 +25,67 @@ class SnapConnector(func: NativeFunctionProvider) {
         (if (body == null) "no body" else "body is a string of length " + body.length)
     )
     val connection: HttpURLConnection = new URL(requestURL).openConnection.asInstanceOf[HttpURLConnection]
-    if (auth != null) {
-      connection.addRequestProperty("Authorization", auth)
-      //func.println("Auth:" + auth)
-    }
-    if (body != null) {
-      connection.setRequestProperty("Content-Type", "application/json")
-      connection.setRequestProperty("Accept", "application/json")
-    }
 
-    connection.setRequestMethod(method)
-
-    connection.setUseCaches(false)
-    connection.setDoInput(true)
-    if (body != null) {
-      connection.setDoOutput(true)
-      val outputInBytes = body.getBytes("UTF-8")
-      func.println(connection.getRequestMethod + "ing " + outputInBytes.length + " bytes...")
-      if (Config.isVerbose) {
-        func.println(connection.getRequestMethod + "ing <" + body + ">")
+    try {
+      if (auth != null) {
+        connection.addRequestProperty("Authorization", auth)
+        //func.println("Auth:" + auth)
       }
-      val os = connection.getOutputStream
-      os.write(outputInBytes)
-    }
-    val timeoutMs = Config.getTimeout
-    connection.setConnectTimeout(timeoutMs)
-    connection.setReadTimeout(timeoutMs)
-    func.println("connect to snapcardster, timeouts (conn/read) " + timeoutMs + " ms...")
-    connection.connect()
+      if (body != null) {
+        connection.setRequestProperty("Content-Type", "application/json")
+        connection.setRequestProperty("Accept", "application/json")
+      }
 
-    val lastCode = connection.getResponseCode
-    func.println(method + " " + requestURL + " response code:" + lastCode)
+      connection.setRequestMethod(method)
 
-    val str =
-      if (lastCode == 200)
-        connection.getInputStream
-      else if (lastCode == 204)
-        new ByteArrayInputStream("204 No Content".getBytes)
-      else
-        connection.getErrorStream
-    if (str == null) {
-      sys.error("Error: " + lastCode)
-    }
-    val rd = new BufferedReader(new InputStreamReader(str))
-    val sb = new StringBuffer
-    var aborted = false
-    while (!aborted) {
-      val line = rd.readLine
-      if (line == null) {
-        aborted = true
-      } else {
-        sb.append(line)
+      connection.setUseCaches(false)
+      connection.setDoInput(true)
+      if (body != null) {
+        connection.setDoOutput(true)
+        val outputInBytes = body.getBytes("UTF-8")
+        func.println(connection.getRequestMethod + "ing " + outputInBytes.length + " bytes...")
+        if (Config.isVerbose) {
+          func.println(connection.getRequestMethod + "ing <" + body + ">")
+        }
+        val os = connection.getOutputStream
+        os.write(outputInBytes)
+      }
+      val timeoutMs = Config.getTimeout
+      connection.setConnectTimeout(timeoutMs)
+      connection.setReadTimeout(timeoutMs)
+      func.println("connect to snapcardster, timeouts (conn/read) " + timeoutMs + " ms...")
+      connection.connect()
+
+      val lastCode = connection.getResponseCode
+      func.println(method + " " + requestURL + " response code:" + lastCode)
+
+      val str =
+        if (lastCode == 200)
+          connection.getInputStream
+        else if (lastCode == 204)
+          new ByteArrayInputStream("204 No Content".getBytes)
+        else
+          connection.getErrorStream
+      if (str == null) {
+        sys.error("Error: " + lastCode)
+      }
+      val rd = new BufferedReader(new InputStreamReader(str))
+      val sb = new StringBuffer
+      var aborted = false
+      while (!aborted) {
+        val line = rd.readLine
+        if (line == null) {
+          aborted = true
+        } else {
+          sb.append(line)
+        }
+      }
+      sb.toString
+    } finally {
+      // https://stackoverflow.com/questions/4767553/safe-use-of-httpurlconnection
+      if (connection != null) {
+        connection.disconnect();
       }
     }
-    sb.toString
   }
 }
